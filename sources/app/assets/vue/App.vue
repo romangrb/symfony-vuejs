e21ad197bff4a211082ab935cf1f99333051cff3
Roman Hrabar <cc.coder1@gmail.com>
2020-01-11 01:46:01 +0200

    added formating

--- sources/app/assets/vue/App.vue
+++ sources/app/assets/vue/App.vue
@@ -14,38 +14,38 @@
 
 
 <script>
-    export default {
-        computed: {
-            isLoggedIn: function() {
-                return this.$store.getters.isLoggedIn;
-            }
-        },
-        methods: {
-            logout: function() {
-                this.$store.dispatch("logout").then(() => {
-                    this.$router.push("/login");
-                });
-            }
-        }
-    };
+  export default {
+    computed: {
+      isLoggedIn: function() {
+          return this.$store.getters.isLoggedIn;
+      }
+    },
+    methods: {
+      logout: function() {
+          this.$store.dispatch("logout").then(() => {
+              this.$router.push("/login");
+          });
+      }
+    }
+  };
 </script>
 
 <style>
-    #app {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-    #nav {
-        padding: 30px;
-    }
-    #nav a {
-        font-weight: bold;
-        color: #2c3e50;
-    }
-    #nav a.router-link-exact-active {
-        color: #42b983;
-    }
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  #nav {
+    padding: 30px;
+  }
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 </style>
