6279c9cc7116399a725c98fe78b8abc93aea7c21
romangrb <grabarroman@gmail.com>
2020-04-16 21:48:08 +0300

    added index

---
+++ sources/app/src/Serializer/Normalizer/PlaceNormalizer.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+/**
+ * Created by PhpStorm.
+ * User: RomanHrb
+ * Date: 06.01.2020
+ * Time: 12:49
+ */
+namespace App\Serializer\Normalizer;
+
+use App\Entity\Place;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class PlaceNormalizer implements NormalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        $data =  [
+            'id' => $object->getId(),
+            'name' => $object->getName(),
+            'description' => $object->getDescription(),
+        ];
+
+        return $data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof Place;
+    }
+}
