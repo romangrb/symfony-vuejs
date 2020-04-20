96b17feb71ae2e06476b30620922179d88cebec1
Yura Pynko <yura.pynko@gmail.com>
2020-04-20 23:56:13 +0300

    Add grapesjs page builder

--- sources/app/assets/vue-argon-dashboard/src/router.js
+++ sources/app/assets/vue-argon-dashboard/src/router.js
@@ -39,6 +39,11 @@ export default new Router({
           path: '/tables',
           name: 'tables',
           component: () => import(/* webpackChunkName: "demo" */ './views/Tables.vue')
+        },
+        {
+          path: '/page-builder',
+          name: 'Page Builder',
+          component: () => import(/* webpackChunkName: "demo" */ './views/PageBuilder.vue')
         }
       ]
     },
