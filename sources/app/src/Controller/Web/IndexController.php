e7476e08d203f205df0599f0153bbbd29bf71b9c
romangrb <grabarroman@gmail.com>
2020-05-22 23:46:49 +0300

    show html

--- sources/app/src/Controller/Web/IndexController.php
+++ sources/app/src/Controller/Web/IndexController.php
@@ -4,19 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller\Web;
 
+use App\Repository\PlaceContentRepository;
 use App\Repository\PlaceRepository;
 use App\Requests\ShowPlaceByLocationRequestValidator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\PlaceContent;
 use Symfony\Component\Filesystem\Filesystem;
 use App\Transformers\ErrorExceptionTransformer;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Twig\Environment;
 
 final class IndexController extends AbstractController
 {
@@ -29,16 +29,25 @@ final class IndexController extends AbstractController
     /** @var SerializerInterface */
     private $serializer;
 
+    /** @var Environment */
+    private $twig;
+
     /**
      * Initiate class properties
      *
      * @param EntityManagerInterface $em
      * @param SerializerInterface $serializer
-     * @param PlaceRepository $repository
+     * @param PlaceContentRepository $repository
+     * @param Environment $twig
      */
-    public function __construct(EntityManagerInterface $em, SerializerInterface $serializer, PlaceRepository $repository)
-    {
+    public function __construct(
+        EntityManagerInterface $em,
+        SerializerInterface $serializer,
+        PlaceContentRepository $repository,
+        Environment $twig
+    ) {
         $this->em = $em;
+        $this->twig = $twig;
         $this->serializer = $serializer;
         $this->repository = $repository;
     }
@@ -49,11 +58,17 @@ final class IndexController extends AbstractController
      * @Route("/place/@{lat},{lng}", name="getPlaceByLocations", methods={"GET"})
      * @param Request $request
      * @param ShowPlaceByLocationRequestValidator $validatorRequest
-     * TODO make response html
      * @param LoggerInterface $logger
      * @return Response
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
-    public function getPlaceByLocations(Request $request, ShowPlaceByLocationRequestValidator $validatorRequest, LoggerInterface $logger)
+    public function getPlaceByLocations(
+        Request $request,
+        ShowPlaceByLocationRequestValidator $validatorRequest,
+        LoggerInterface $logger): Response
     {
         $lat = (float) $request->get('lat');
         $lng = (float) $request->get('lng');
@@ -63,40 +78,39 @@ final class IndexController extends AbstractController
             'lng' => $lng,
         ];
 
-        $validatedRequest = $validatorRequest->validate($input);
-
-        if ($validatedRequest) {
+        if ($validatorRequest->validate($input)) {
             return new Response('',Response::HTTP_NOT_FOUND);
         }
 
-        $placeContent = $this->em->getRepository(PlaceContent::class)->getPlaceContentByLocation($lat, $lng);
+        $placeContent = $this->repository->getPlaceContentByLocation($lat, $lng);
 
         if (! $placeContent || ! $placeContent->getIsPublished()) {
             return new Response('',Response::HTTP_NOT_FOUND);
         }
 
-        $data = [
-            'gjs-css'  => '',
-            'gjs-html' => ''
-        ];
+        $content = '';
 
         try {
-//            TODO:: move to one class to use on 2 routes
             $filesystem = new Filesystem();
             $path = $this->getParameter('kernel.project_dir') . "/var/places/{$placeContent->getPlace()->getId()}/";
 
             $file_name = $placeContent->getFileName();
 
-            if ($file_name && $filesystem->exists("$path/" . $file_name)
-            ) {
-                $data['gjs-html'] = file_get_contents($path . $file_name);
+            if ($file_name && $filesystem->exists("{$path}/{$file_name}")) {
+                $content = file_get_contents($path . $file_name);
             }
+
         } catch (\Exception $e) {
             $message = ErrorExceptionTransformer::transform($e);
             $logger->error(print_r($message, true));
             return new Response('',Response::HTTP_NOT_FOUND);
         }
-// TODO merge to one file return HTML
-        return new Response($data, Response::HTTP_OK, []);
+
+        $contents = $this->twig->render('place.html.twig', [
+            'title' => $placeContent->getPlace()->getName(),
+            'content' => $content,
+        ]);
+
+        return new Response($contents);
     }
 }
